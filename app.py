73ffaa5395c473146c1169020a9e562a7bc1e140
sarahgalves <sarahgabriellla@gmail.com>
2026-03-26 13:59:58 -0700

    initial commit

--- app.py
+++ app.py
@@ -3,3 +3,7 @@ import pandas as pd
 import plotly.express as px
 
 car_data = pd.read_csv('c:/Users/sarah/OneDrive/Documents/GitHub/spr5_projeto/vehicles.csv') # lendo os dados
+
+st.header('Jogando uma moeda')
+
+st.write('Ainda não é um aplicativo funcional. Em construção.')
