72ec70cd628dbb178c37c25b72b51f94d6f222c4
sarahgalves <sarahgabriellla@gmail.com>
2026-03-26 14:15:37 -0700

    teste

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-car_data = pd.read_csv('c:/Users/sarah/OneDrive/Documents/GitHub/spr5_projeto/vehicles.csv') # lendo os dados
+#car_data = pd.read_csv('c:/Users/sarah/OneDrive/Documents/GitHub/spr5_projeto/vehicles.csv') # lendo os dados
 
 st.header('Jogando uma moeda')
 
