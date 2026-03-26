979d4c8bebc3f5f604ddee6b369da9eb5746102b
sarahgalves <sarahgabriellla@gmail.com>
2026-03-26 13:36:32 -0700

    primeiro dia

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+car_data = pd.read_csv('c:/Users/sarah/OneDrive/Documents/GitHub/spr5_projeto/vehicles.csv') # lendo os dados
